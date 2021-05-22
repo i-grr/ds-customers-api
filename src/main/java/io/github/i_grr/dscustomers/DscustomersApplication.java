2aa48f8f445cc7776fe5d3c320a338354114f4b5
Igor Pereira <igorpereira120@gmail.com>
2021-05-22 14:05:01 -0300

    Project created

---
+++ src/main/java/io/github/i_grr/dscustomers/DscustomersApplication.java
@@ -0,0 +1,13 @@
+package io.github.i_grr.dscustomers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscustomersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/i_grr/dscustomers/DscustomersApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.i_grr.dscustomers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscustomersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
